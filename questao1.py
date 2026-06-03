3df9f80eadb5d6143eb3b7fe17ffcdf606c3c55d
Raniele2000 <barros.raniele@aluno.ufca.edu.br>
2026-06-03 12:26:13 +0000

    subindo questao 1

---
+++ questao1.py
@@ -0,0 +1,8 @@
+#(1)Imagine que nós aplicamos o procedimento Partição() sobre a lista L
+#letra(a):
+O melhor caso acontece quando a partição divide a lista em duas partes equilibradas.
+Nesse caso,o tempo de execução é o(n**2).
+#letra(b):
+O pior caso ocorre quando a partição fica totalmente desbalanceada.
+nesse caso,otempo de execução tambem é o(n**2).
+
