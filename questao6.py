2f9a548e7a91553228bb3d181018229b35d0c515
Raniele2000 <barros.raniele@aluno.ufca.edu.br>
2026-06-03 12:11:30 +0000

    subindo questao 6

---
+++ questao6.py
@@ -0,0 +1,13 @@
+#(6)Permutações
+U=[1,2,3,4,5,6,7,8,9,10]
+V=[7,2,3,1,6,5,9,10,4,8]
+igual = True
+for i in U :
+   if i not in v:
+      igual = False
+if igual :
+  print("são permutações")
+else:
+  print("não são")
+
+
